e8fe068c1b0940fbdb511cf3ed7c80d3fce96158
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-06 08:12:35 +0700

    nambahin composable tadi ketinggalan

--- app/src/main/java/com/example/inputpengguna/FormPendaftaran.kt
+++ app/src/main/java/com/example/inputpengguna/FormPendaftaran.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@Composable
 fun FormatPendaftaran(modifier: Modifier) {
 
 }
